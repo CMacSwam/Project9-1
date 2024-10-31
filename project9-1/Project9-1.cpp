546004f8c9309ccac241228526ea64a5d850eac5
CMacSwam <c-mccluskey.1@onu.edu>
2024-10-31 08:44:45 -0400

    Can now display every element in order of appearance in the array

--- project9-1/Project9-1.cpp
+++ project9-1/Project9-1.cpp
@@ -1,2 +1,24 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+
+	int array[10];
+	srand(static_cast<unsigned int>(time(0)));
+
+	for (int i = 0; i < 10; i++) {
+
+		array[i] = rand() % 10000 + 1000;
+
+		cout << "Element in Array: ";
+		cout << array[i] << " ";
+
+		
+	}
+	
+
+}
 
